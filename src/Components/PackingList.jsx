501253e75eaa6f46c5d839f2dfcaf4700a2092d4
Atul Yadav <atuly3051@gmail.comm>
2024-09-30 00:34:39 +0530

    Updated Packing Comp

--- src/Components/PackingList.jsx
+++ src/Components/PackingList.jsx
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import List_Item from "./List_Item";
-const PackingList = ({item,handle_delete,handle_change}) => {
+const PackingList = ({ item, handle_delete, handle_change }) => {
+  const [sort, sortby] = useState("input");
+  let sortItem;
+  if (sort === "input") sortItem = item;
+
+  if (sort === "des")
+    sortItem = item
+      .slice()
+      .sort((a, b) => a.des.localeCompare(b.des));
+
+  if (sort==="packed"){
+    sortItem = item.slice().sort((a,b)=> Number(a.packed)-Number(b.packed))
+  }
+
   return (
     <div className="list">
       <ul>
-        {item.map((item,idx) => (
-          <List_Item key={idx} item={item} handle_delete={handle_delete}  handle_change={handle_change} />
+        {sortItem.map((item, idx) => (
+          <List_Item
+            key={idx}
+            item={item}
+            handle_delete={handle_delete}
+            handle_change={handle_change}
+          />
         ))}
       </ul>
-      <select>
-        <optgroup label="SORT BY INPUT ORDER">
-          <option value="1">1</option>
-          <option value="2">2</option>
-        </optgroup>
-      </select>
+      <div className="action">
+        <select value={sort} onChange={(e) => sortby(e.target.value)}>
+          <option value="input">SORT BY INPUT ORDER</option>
+          <option value="des">SORT BY Description</option>
+          <option value="packed">SORT BY PACKED ITEM</option>
+        </select>
+      </div>
     </div>
   );
 };
