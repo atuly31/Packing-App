01db3eafecf8e501aac0bedf214eaf6d4b23c2e8
Atul Yadav <atuly3051@gmail.comm>
2024-09-30 00:35:09 +0530

    Updated Stats Comp

--- src/Components/Stats.jsx
+++ src/Components/Stats.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
+import React from "react";
 
-const Stats = ({item}) => {
-  
+const Stats = ({ item }) => {
   const numItem = item.length;
-  const numPacked = item.filter((val)=>
-    val.packed === true
-  ).length;
-  const percent = (numPacked/numItem)*100
+  const numPacked = item.filter((val) => val.packed === true).length;
+  
+  const percent = numItem ===0 ? 0 : Math.round((numPacked / numItem)) * 100;
+  
   return (
-    <div className="stats">
-       You have {numItem} items on your list, and you already packed {numPacked}({percent}%)
-    </div>
-  )
-}
+    <footer className="stats">
+      <em>
+        {percent === 100
+          ? " You have everthing ready to go ✈️"
+          : `💼You have ${numItem} items on your list, and you already packed ${numPacked}(${percent}%)`}
+      </em>
+    </footer>
+  );
+};
 
-export default Stats
+export default Stats;
